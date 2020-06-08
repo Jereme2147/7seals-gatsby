2cf7f43a6f4d68bbf0ead9cde150996b5c037840
jereme <jereme.daniels@gmail.com>
2020-06-08 17:34:03 -0400

    weird errors

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,13 +8,12 @@
 //IMPORTANT build with - gatsby build --prefix-paths -Also, I just cloned the public folder and it seems to have worked fine. 
 //need to figure out how the fuck it's sorting.  Commented out all the date lines.
 module.exports = {
-  
   // pathPrefix: '/posts',
-  pathPrefix: '7seals/',
+  pathPrefix: "7seals/",
   /* Your site config here */
   siteMetadata: {
-    title: '7Seals CrossFit Coaches blog',
-    author: 'Jereme Daniels'
+    title: "7Seals CrossFit Coaches blog",
+    author: "Jereme Daniels",
   },
   plugins: [
     {
@@ -22,42 +21,44 @@ module.exports = {
       options: {
         fonts: [
           {
-            family: `Staatliches`
+            family: `Staatliches`,
           },
           {
-            family: `McLaren`
+            family: `McLaren`,
           },
           {
-            family: 'ZCOOL XiaoWei'
-          }
+            family: "ZCOOL XiaoWei",
+          },
         ],
-      }
+      },
     },
     "gatsby-plugin-catch-links",
-     "gatsby-plugin-react-helmet",
-    'gatsby-plugin-sass',//obviously for sass
-    { //this is the plugin that retrieves local files. Found in graphql
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sass", //obviously for sass
+    {
+      //this is the plugin that retrieves local files. Found in graphql
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
-        path: `${__dirname}/src/`
-      }
+        path: `${__dirname}/src/`,
+      },
     },
+    `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
-          'gatsby-remark-relative-images',
+          "gatsby-remark-relative-images",
           {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 900,
-              linkImagesToOriginal: false
+              linkImagesToOriginal: false,
             },
           },
         ],
       },
     },
-  ]
+  ],
 }
